0fe79bf27519fcc36763906e70b372feb2821952
Elias Moura <d.eliasmoura2@gmail.com>
2020-06-20 12:39:58 -0300

    se anulo la modificacion de la ruta para llamar a salir.php en el navbar

--- php/NavBar.php
+++ php/NavBar.php
@@ -82,7 +82,7 @@
       <form class="form-inline my-2 mr-3 my-lg-0">
         <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
         <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
-        <a class="btn btn-danger" href="salir.php">Salir</a>
+        <a class="btn btn-danger" href="php/salir.php">Salir</a>
       </form>
     </div>    
   </div>

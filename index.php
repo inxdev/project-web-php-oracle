90d4b089401f0cf9e85dfd5a38892594e9e6b553
Elias Moura <d.eliasmoura2@gmail.com>
2020-06-20 07:42:38 -0300

    primer commit desarrollo index

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="Moura" content="">
+
+  <title>Inicio TP FINAL</title>
+
+      <!-- Bootstrap CSS CDN -->
+      <link 
+      rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+      crossorigin="anonymous">
+
+
+  <!-- Custom fonts for this template -->
+  <link href="assets/css/all.css" rel="stylesheet">
+  <link href="assets/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template -->
+  <link href="assets/css/landing-page.css" rel="stylesheet">
+  <!-- <link href="css/styles.css" rel="stylesheet" /> -->
+  <!-- <link rel="stylesheet" href="assets/css/inscripcion.css"> -->
+
+</head>
+
+<body id="page-top">
+
+  <nav class="navbar navbar-expand-lg sticky-top navbar navbar-dark bg-dark"  id="mainNav">
+    <a class="navbar-brand js-scroll-trigger mr-sm-5 mb-0 " href="#page-top">Sistema prestamos</a>
+    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> Menu 
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav justify-content-center mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#page-top"> Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#team"> Team </a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#escenario"> Escenario </a>
+        </li>
+      </ul>
+          <form class="form-inline my-2 mr-3 my-lg-0">
+            <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+            <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+            <a class="btn btn-primary" href="1-login.php">Sign In</a>
+          </form>
+          <form class="form-inline my-2 my-lg-0">
+          <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+            <a class="btn btn-secondary" disabled>Sign Up</a>
+            <!-- href="2-singup.php" -->
+          </form>
+    </div>
+  </nav>
+
+  <header class="masthead text-white text-center" id="Presentacion">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h1 class="mb-5" id="txt1">Proyecto Final 2020</h1>
+          <h2 class="mb-3" id="txt1">Tecnologias de bases de datos</h2>
+          <h3 class="mb-3" id="txt1">Universidad Gaston Dachary</h3>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  
+  <section class="features-icons p-md-4 p-lg-5 text-center bg-light" id="team">
+    <div class="container p-lg-4">
+      <div class="row justify-content-center mb-3">
+        <div class="col-lg-4"> 
+        <div class="features-icons-item mx-auto mb-lg-0 mb-lg-2">
+          <div class="features-icons-icon">
+            <i class="icon-people m-auto text-primary"></i>
+          </div>
+          <h2 class="">Profesores</h2>
+        </div>
+        </div>
+      </div>
+      <div class="row justify-content-center mb-2">
+        <div class="col-lg-4">
+          <div class="mx-auto">
+            <!-- <img class="img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt=""> -->
+            <h3>Ing. Suenaga, Roberto</h3>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="mx-auto">
+            <!-- <img class="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt=""> -->
+            <h3>Ing. Barreiro, Enrique</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container p-lg-4">
+      <div class="row justify-content-center mb-3">
+        <div class="col-lg-4"> 
+        <div class="features-icons-item mx-auto mb-lg-0 mb-lg-2">
+          <div class="features-icons-icon">
+            <i class="icon-people m-auto text-primary"></i>
+          </div>
+          <h2 class="mb-5">Alumnos</h2>
+        </div>
+        </div>
+      </div>
+      <div class="row justify-content-center mb-2">
+        <div class="col-lg-4">
+          <div class="mx-auto" >
+            <!-- <img class="img-fluid rounded-circle mb-3" src="img/testimonials-1.jpg" alt=""> -->
+            <h3><a href="">Moura, Damian Elias</a></h3>
+            <h5>Matricula: 66422</h5>
+            <!-- <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p> -->
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="mx-auto" >
+            <!-- <img class="img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt=""> -->
+            <h3><a href="">Iriarte, Jose</a></h3>
+            <h5>Matricula: 66</h5>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="mx-auto">
+            <!-- <img class="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt=""> -->
+            <h3><a href="">Ferreyra, Dario</a></h3>
+            <h5>Matricula: 66</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <!-- Image Showcases -->
+  <section class="showcase" id="escenario">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+        <!-- <div class="col-lg-8 order-lg-2 text-white showcase-img" style="background-image: url('img/0-backup.png');"></div> -->
+        <div class=" my-auto showcase-text">
+          <h2>Escenario del proyecto</h2>
+          <p class="lead mb-0">
+            En este proyecto se nos plantea un escenario donde una empresa privada decide realizar Préstamos Personales,
+            la misma requiere de un sistema de base de datos que le permita la gestión de los mismos.<br>
+            Para el desarrollo del proyecto se realizo el siguiente Diagrama Entidad Relacion (DER)
+          </p>
+        </div>
+        <div class="row no-gutters">
+          <img class="showcase-img img-fluid img-thumbnail" src="assets/img/0-backup.png" alt="DER proyecto Final Tecnologias de bases de datos">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+    <div class="container text-center">
+        <!-- <small>Copyright &copy; Your Website</small> -->
+        <small>Universidad Gaston Dachary - Tecnologias de Base de datos</small>
+    </div>
+  </footer>
+  
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>

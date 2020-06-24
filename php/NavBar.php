b4316768b3d4b347c8d9874f4584b050d5f82347
Elias Moura <d.eliasmoura2@gmail.com>
2020-06-24 00:48:39 -0300

    modificaciones en los menus del navBar y en los submenus para el  llamado a las paginas

--- php/NavBar.php
+++ php/NavBar.php
@@ -1,13 +1,24 @@
+<!-- NOTE Menu de navegacion superior de la pagina web -->
 <nav class="navbar navbar-expand-lg sticky-top navbar navbar-dark bg-dark" id="mainNav">
-  <a class="navbar-brand js-scroll-trigger mr-sm-5 mb-0 " href="#page-top">Sistema prestamos</a>
+
+  <!-- NOTE Nombre del sistema que aparece en el menu superior a la izquierda -->
+  <a class="navbar-brand js-scroll-trigger mr-sm-5 mb-0 " href="#page-top">Sistema de prestamos</a>
+
+  <!-- NOTE boton que aparecera para desplegar el menu cuando la pantalla se achique -->
   <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> Menu
     <span class="navbar-toggler-icon"></span>
   </button>
+
+  <!-- NOTE Botones del menu superior -->
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav justify-content-center mr-auto">
+
+      <!-- NOTE botton para ir a la pagina principal -->
       <li class="nav-item active">
         <a class="nav-link" href="3-paginaPrincipal.php"> Home <span class="sr-only">(current)</span></a>
       </li>
+
+      <!-- NOTE boton que despliega el submenu de clientes -->
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Clientes
@@ -24,6 +35,8 @@
           <a class="dropdown-item" href="#">Something</a>
         </div>
       </li>
+
+      <!-- NOTE boton que despliega el submenu de Prestamos -->
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Prestamos
@@ -38,12 +51,14 @@
           <a class="dropdown-item" href="#">Something</a>
         </div>
       </li>
+
+      <!-- NOTE boton que despliega el submenu de Convenios -->
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Convenios
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Inscripcion</a>
+          <a class="dropdown-item" href="7-paginaAltaConvenio.php">Inscripcion</a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#">Modificacion</a>
           <div class="dropdown-divider"></div>
@@ -51,14 +66,15 @@
         </div>
       </li>
 
+      <!-- NOTE  boton que despliega el submenu de Informes -->
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Informes
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Lista de clientes</a>
+          <a class="dropdown-item" href="9-pagListadoPersonas.php">Lista de personas</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Lista de titulares</a>
+          <a class="dropdown-item" href="10-pagListadoTitulares.php">Lista de Titulares</a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#">Lista de garantes</a>
           <div class="dropdown-divider"></div>
@@ -80,6 +96,7 @@
         </div>
       </li> -->
 
+      <!-- NOTE seccion que muestra el usuario conectado -->
       <ul class="navbar-nav ml-lg-5 ml-sm-0 mr-auto">
         <li class="nav-item">
           <div class="text-light">
@@ -88,10 +105,10 @@
         </li>
       </ul>
     </ul>
+
+    <!-- NOTE codigo del boton de salida del sistema -->
     <div class="row justify-content-end">
       <form class="form-inline my-2 mr-3 my-lg-0">
-        <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
-        <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
         <a class="btn btn-danger" href="php/salir.php">Salir</a>
       </form>
     </div>
